bc244386d21d5d2709a9ced2b4b511778e95a740
Johnnatan <romerozapatajonathan@gmail.com>
2024-04-14 17:43:08 -0400

    JMS 1.0 fundamentals

---
+++ jmsFundamentals/src/test/java/com/example/jmsFundamentals/JmsFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmsFundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
